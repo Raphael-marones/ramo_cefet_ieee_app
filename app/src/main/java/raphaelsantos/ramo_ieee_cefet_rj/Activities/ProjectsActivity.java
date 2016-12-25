bed9eea8465381c248237e6312349714320aa637
raphaelsantos <raphaelmcsantos.santos@gmai.com>
2016-12-25 10:15:17 -0200

    SettingUpConnections

--- app/src/main/java/raphaelsantos/ramo_ieee_cefet_rj/Activities/ProjectsActivity.java
+++ app/src/main/java/raphaelsantos/ramo_ieee_cefet_rj/Activities/ProjectsActivity.java
@@ -43,27 +43,27 @@ public class ProjectsActivity  extends AppCompatActivity{
 
     private TextView mobile_title;
     private TextView mobile_members;
-    private TextView mobile_leaader;
+    private TextView mobile_content;
 
     private TextView game_title;
     private TextView game_members;
-    private TextView game_leader;
+    private TextView game_content;
 
     private TextView solar_title;
     private TextView solar_members;
-    private TextView solar_leader;
+    private TextView solar_content;
 
     private TextView schoolieee_title;
     private TextView schoolieee_members;
-    private TextView schoolieee_leader;
+    private TextView schoolieee_content;
 
     private TextView sumo_title;
     private TextView sumo_members;
-    private TextView sumo_leader;
+    private TextView sumo_content;
 
     private TextView trekking_title;
     private TextView trekking_members;
-    private TextView trekking_leader;
+    private TextView trekking_content;
 
     private TextView competition_title;
     private TextView competition_members;
@@ -71,7 +71,8 @@ public class ProjectsActivity  extends AppCompatActivity{
 
     private TextView linefollower_title;
     private TextView linefollower_members;
-    private TextView linefollower_leader;
+    private TextView linefollower_content;
+
     long counting = 0;
     private String titulo_teste = new String();
     public  Bundle datafromdatabase_coders;
@@ -86,7 +87,35 @@ public class ProjectsActivity  extends AppCompatActivity{
         competition_title = (TextView)findViewById(R.id.title_competition);
         competition_members = (TextView)findViewById(R.id.members_competition);
         competition_content = (TextView)findViewById(R.id.content_competition);
+
+        mobile_title = (TextView)findViewById(R.id.title_mobile);
+        mobile_content = (TextView)findViewById(R.id.content_mobile);
         mobile_members = (TextView) findViewById(R.id.members_mobile);
+
+        game_title = (TextView) findViewById(R.id.title_game);
+        game_content = (TextView) findViewById(R.id.content_game);
+        game_members = (TextView) findViewById(R.id.members_game);
+
+        linefollower_title = (TextView) findViewById(R.id.title_linefollower);
+        linefollower_content = (TextView) findViewById(R.id.content_linefollower);
+        linefollower_members = (TextView) findViewById(R.id.members_linefollower);
+
+        sumo_title = (TextView) findViewById(R.id.title_sumo);
+        sumo_content = (TextView) findViewById(R.id.content_sumo);
+        sumo_members = (TextView) findViewById(R.id.members_sumo);
+
+        trekking_title = (TextView) findViewById(R.id.title_trekking);
+        trekking_content = (TextView) findViewById(R.id.content_trekking);
+        trekking_members = (TextView) findViewById(R.id.members_Trekking);
+
+        schoolieee_title = (TextView) findViewById(R.id.title_schoolieee);
+        schoolieee_content = (TextView) findViewById(R.id.content_schoolieee);
+        schoolieee_members = (TextView) findViewById(R.id.members_schoolieee);
+
+        solar_title = (TextView) findViewById(R.id.title_solartree);
+        solar_content = (TextView) findViewById(R.id.content_solartree);
+        solar_members = (TextView) findViewById(R.id.members_solartree);
+
         applyingforbutton = (Button) findViewById(R.id.applyingfor);
 
 //        Intent i = getIntent();
@@ -100,9 +129,9 @@ public class ProjectsActivity  extends AppCompatActivity{
 
         DatabaseReference acesso_school = myRef.child("Projects").child("Schoolers");
 
-        DatabaseReference acesso_solar = myRef.child("Projects").child("Solar");
+        DatabaseReference acesso_solar = myRef.child("Projects").child("Solars");
 
-        DatabaseReference acesso_robot = myRef.child("Projects").child("Robot");
+        DatabaseReference acesso_robot = myRef.child("Projects").child("Robotics");
 
         DatabaseReference acesso_coder = myRef.child("Projects").child("Coders");
 
@@ -271,20 +300,20 @@ public class ProjectsActivity  extends AppCompatActivity{
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
-                trekking_robot.setTitle(dataSnapshot.child("Trekking").child("Title").getValue(String.class));
-                trekking_robot.setContent(dataSnapshot.child("Trekking").child("Content").getValue(String.class));
-                trekking_robot.setLeaders(dataSnapshot.child("Trekking").child("Leaders").getValue(String.class));
-                trekking_robot.setTeam_members(dataSnapshot.child("Trekking").child("Participant").getValue(String.class));
+                trekking_robot.setTitle(dataSnapshot.child("Autonomous").child("Title").getValue(String.class));
+                trekking_robot.setContent(dataSnapshot.child("Autonomous").child("Content").getValue(String.class));
+                trekking_robot.setLeaders(dataSnapshot.child("Autonomous").child("Leaders").getValue(String.class));
+                trekking_robot.setTeam_members(dataSnapshot.child("Autonomous").child("Participant").getValue(String.class));
 
                 sumo_robot.setTitle(dataSnapshot.child("Sumo").child("Title").getValue(String.class));
                 sumo_robot.setContent(dataSnapshot.child("Sumo").child("Content").getValue(String.class));
                 sumo_robot.setLeaders(dataSnapshot.child("Sumo").child("Leaders").getValue(String.class));
                 sumo_robot.setTeam_members(dataSnapshot.child("Sumo").child("Participant").getValue(String.class));
 
-                linefollower_robot.setTitle(dataSnapshot.child("LineFollower").child("Title").getValue(String.class));
-                linefollower_robot.setContent(dataSnapshot.child("LineFollower").child("Content").getValue(String.class));
-                linefollower_robot.setLeaders(dataSnapshot.child("LineFollower").child("Leaders").getValue(String.class));
-                linefollower_robot.setTeam_members(dataSnapshot.child("LineFollower").child("Participant").getValue(String.class));
+                linefollower_robot.setTitle(dataSnapshot.child("Line_Follower").child("Title").getValue(String.class));
+                linefollower_robot.setContent(dataSnapshot.child("Line_Follower").child("Content").getValue(String.class));
+                linefollower_robot.setLeaders(dataSnapshot.child("Line_Follower").child("Leaders").getValue(String.class));
+                linefollower_robot.setTeam_members(dataSnapshot.child("Line_Follower").child("Participant").getValue(String.class));
             }
 
             @Override
@@ -319,10 +348,10 @@ public class ProjectsActivity  extends AppCompatActivity{
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
-                solar.setTitle(dataSnapshot.child("Solars").child("Title").getValue(String.class));
-                solar.setContent(dataSnapshot.child("Solars").child("Content").getValue(String.class));
-                solar.setLeaders(dataSnapshot.child("Solars").child("Leaders").getValue(String.class));
-                solar.setTeam_members(dataSnapshot.child("Solars").child("Participant").getValue(String.class));
+                solar.setTitle(dataSnapshot.child("Title").getValue(String.class));
+                solar.setContent(dataSnapshot.child("Content").getValue(String.class));
+                solar.setLeaders(dataSnapshot.child("Leaders").getValue(String.class));
+                solar.setTeam_members(dataSnapshot.child("Participant").getValue(String.class));
             }
 
 
@@ -335,20 +364,20 @@ public class ProjectsActivity  extends AppCompatActivity{
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
-                Log.w(TAG, "Value is: " + competition_coder.getTitle());
-                Log.w(TAG, "Value is: " + competition_coder.getContent());
-                Log.w(TAG, "Value is: " + competition_coder.getLeaders());
-                Log.w(TAG, "Value is: " + competition_coder.getTeam_members());
-                Log.w(TAG, "Value is: " + competition_coder.getSponsors());
+                Log.w(TAG, "Value is: " + solar.getTitle());
+                Log.w(TAG, "Value is: " + solar.getContent());
+                Log.w(TAG, "Value is: " + solar.getLeaders());
+                Log.w(TAG, "Value is: " + solar.getTeam_members());
+                Log.w(TAG, "Value is: " + solar.getSponsors());
 
 
             }
-        }, 1100);
+        }, 1800);
 
         //This timer gives time for the Thread to be loaded with
         // the desired information from firebase
 
-        new CountDownTimer(1500, 1000) {
+        new CountDownTimer(1600,1000) {
 
             public void onTick(long millisUntilFinished) {
             }
@@ -357,6 +386,28 @@ public class ProjectsActivity  extends AppCompatActivity{
                 competition_title.setText(competition_coder.getTitle());
                 competition_content.setText(competition_coder.getContent());
                 competition_members.setText(competition_coder.getTeam_members());
+                mobile_title.setText(mobile_coder.getTitle());
+                mobile_content.setText(mobile_coder.getContent());
+                mobile_members.setText(mobile_coder.getTitle());
+                game_title.setText(game_coder.getTitle());
+                game_content.setText(game_coder.getContent());
+                game_members.setText(game_coder.getTeam_members());
+                trekking_title.setText(trekking_robot.getTitle());
+                trekking_content.setText(trekking_robot.getContent());
+                trekking_members.setText(trekking_robot.getTeam_members());
+                sumo_title.setText(sumo_robot.getTitle());
+                sumo_content.setText(sumo_robot.getContent());
+                sumo_members.setText(sumo_robot.getTeam_members());
+                linefollower_title.setText(linefollower_robot.getTitle());
+                linefollower_content.setText(linefollower_robot.getContent());
+                linefollower_members.setText(linefollower_robot.getTeam_members());
+                solar_title.setText(solar.getTitle());
+                solar_content.setText(solar.getContent());
+                solar_members.setText(solar.getTeam_members());
+                schoolieee_title.setText(school.getTitle());
+                schoolieee_content.setText(school.getContent());
+                schoolieee_members.setText(school.getTeam_members());
+
             }
         }.start();
 
